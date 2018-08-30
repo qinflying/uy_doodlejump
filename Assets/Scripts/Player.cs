02dfa95c7fa86771b4616ca3019fc5a73748cfad
qinflying <qinflying@outlook.com>
2018-08-31 00:07:35 +0800

    按键控制角色水平移动
    Signed-off-by: qinflying <qinflying@outlook.com>

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,8 +11,26 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        OnHorizontalControl();
 	}
+
+    //水平移动控制
+    void OnHorizontalControl() {
+        Vector3 acc = Vector3.zero;
+        Vector3 diff;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            acc.x = -0.1f;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            acc.x = 0.1f;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        Debug.Log(Time.time);
+        diff = Vector3.MoveTowards(transform.localPosition, transform.localPosition + acc, 0.5f * Time.time);
+        transform.localPosition = diff;
+    }
 
     void OnTriggerEnter2D(Collider2D oCollider) {
         if (oCollider.tag == "platform") {
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Unity learn: doodle jump
 
 
 
-1.分相机渲染
+#### 1.分相机渲染
 
 a.主摄像机不看Water层
 
@@ -13,14 +13,30 @@ c.主摄像机Clear Flags 选择Depth only(为了2个相机的看到的内容融
 
 
 
-2.贴合屏幕宽度计算
+#### 2.贴合屏幕宽度计算
 
 ![](./README/cameracal.png)
 
 
 
-3.Rigidbody2D刚体运动相关
+#### 3.Rigidbody2D刚体运动相关
 
 a.AddForce方法
 
 b.ForceMode2D枚举
+
+
+
+#### 4.按键控制
+
+角色左右移动
+
+a.Vector3.MoveTowards进行插值移动
+
+b.翻转：使用localScale中x参数正负值翻转
+
+c.Input.GetKey获得持续按键状态
+
+d.Time.time?
+
+e.翻转时的偏移问题，重心不对称;编辑图片的中心SpriteEditor
