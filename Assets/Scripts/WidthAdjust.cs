3e522785323e7efa89f3f65eb8632ba43c3e1b58
qinflying <qinflying@outlook.com>
2018-08-30 23:25:36 +0800

    代码自适应图片宽度
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/WidthAdjust.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WidthAdjust : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Resize();
+	}
+
+    void Resize() {
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        float targetWidth = 2 * Camera.main.orthographicSize * Screen.width / Screen.height;
+        Vector3 scale = transform.localScale;
+        scale.x = targetWidth / width;
+        transform.localScale = scale;
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ a.主摄像机不看Water层
 b.背景相机看Water层（背景设置为Water层）
 
 c.主摄像机Clear Flags 选择Depth only(为了2个相机的看到的内容融合？)
+
+
+
+2.贴合屏幕宽度计算
+
+![](./README/cameracal.png)
