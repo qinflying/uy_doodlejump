7bc626321267b033067eca5935f27fc34c87df8c
qinflying <qinflying@outlook.com>
2018-08-30 23:42:45 +0800

    角色弹跳功能
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D oCollider) {
+        if (oCollider.tag == "platform") {
+            ToJumpAction();
+        }
+    }
+
+    void ToJumpAction(float factor = 1f) {
+        //跳起之前清空之前的速度
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 12 * factor), ForceMode2D.Impulse);
+    }
+}
--- README.md
+++ README.md
@@ -16,3 +16,11 @@ c.主摄像机Clear Flags 选择Depth only(为了2个相机的看到的内容融
 2.贴合屏幕宽度计算
 
 ![](./README/cameracal.png)
+
+
+
+3.Rigidbody2D刚体运动相关
+
+a.AddForce方法
+
+b.ForceMode2D枚举
